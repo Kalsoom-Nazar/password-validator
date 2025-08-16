465ab5d1055b5d01ee0b73b5443596dfa5ab69e2
Kalsoom Nazar <kalsoomnazar.dev@gmail.com>
2025-08-15 21:52:12 -0700

    Update password.py

--- password.py
+++ password.py
@@ -1,19 +1,3 @@
-# Tum ek freelance developer ho, client ne bola:
-# "Ek system banao jo check kare ke entered password strong hai ya weak."
-
-# Rules for strong password:
-
-# Kam se kam 8 characters ka ho
-
-# Kam se kam 1 uppercase letter ho
-
-# Kam se kam 1 lowercase letter ho
-
-# Kam se kam 1 number ho
-
-# Tumhara task:
-# Python me ek program likho jo user ka password le aur bole:
-# Strong Password ya Weak Password.
 password=input("Enter your Password here ")
 has_upper=False
 has_lower=False
@@ -31,6 +15,7 @@ if len(password)>=8 and has_digit and has_lower and has_upper:
 else:
     print("Password is weak")   
               
+
 
 
 
